d6ab14352bc2058ee022cad3d6a310c7a7407092
giaco <greco.giacomo56@gmail.com>
2021-07-09 21:39:41 +0200

    Lab 6 temp part 2

--- node.py
+++ node.py
@@ -9,6 +9,7 @@ class Node:
 
         self._label = str(attr_dict.get('label'))
         self._successive = {}  # dictionary of lines
+        self._switching_matrix = None
         self.lenght
 
     @property
@@ -22,6 +23,14 @@ class Node:
     @property
     def lenght(self):
         return self._label
+
+    @property
+    def switching_matrix(self):
+        return self._switching_matrix
+
+    @switching_matrix.setter
+    def switching_matrix(self, switching_matrix):
+        self._switching_matrix = switching_matrix
 
     @label.setter
     def lenght(self, lenght):
@@ -40,7 +49,17 @@ class Node:
 
     if len(path) > 1:
         line_label = path[:2]
-    line = self.successive[line_label]
-    lightpath.next()
-    lightpath = line.propagate(lightpath, occupation)
+        line = self.successive[line_label]
+        lightpath.next()
+        lightpath = line.propagate(lightpath, occupation)
     return lightpath
+    def update_switching_matrix(self, channel: int, src: str, dst: str, state: int):
+        """
+        Updates the switching matrix
+
+        :param channel: Number of the channel
+        :param src: Source node label
+        :param dst: Destination node label
+        :param state: 0 is occupied, 1 is free
+        """
+        self._switching_matrix[src][dst][channel] = state
