e32972509d33ea8e2ae92da387a67860d1fa8239
giaco <greco.giacomo56@gmail.com>
2021-07-10 10:30:44 +0200

    Lab 8 part 1

--- line.py
+++ line.py
@@ -5,12 +5,40 @@ class Line ( object ):
         self._length = line_dict['length ']
         self._state = ['free '] * 10
         self._successive = {}
+        self._n_amplifiers = 0
+        self._gain = 16  # dB
+        self._noise_figure = 3  # dB
     @property
     def label ( self ):
         return self . _label
     @property
     def length ( self ):
         return self . _length
+
+    @property
+    def n_amplifiers(self):
+        return self._n_amplifiers
+
+    @n_amplifiers.setter
+    def n_amplifiers(self, n_amplifiers: int):
+        self._n_amplifiers = n_amplifiers
+
+    @property
+    def gain(self):
+        return self._gain
+
+    @gain.setter
+    def gain(self, gain: float):
+        self._gain = gain
+
+    @property
+    def noise_figure(self):
+        return self._noise_figure
+
+    @noise_figure.setter
+    def noise_figure(self, noise_figure: float):
+        self._noise_figure = noise_figure
+
     @property
     def state ( self ):
         return self . _state
@@ -30,6 +58,7 @@ class Line ( object ):
     @successive.setter
     def successive(self, successive):
         self._successive = successive
+
 
     def latency_generation(self):
         latency = self.length / (c * 2 / 3)
